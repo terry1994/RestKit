0ec336fe267371f15a0f80d1a5d74b314631b329
Terry Lewis <terry1994dev@gmail.com>
2017-08-18 00:31:26 +1000

    Arr framework compiling on iOS and tvOS.

---
+++ SOCKit-iOS/SOCKit-iOS.h
@@ -0,0 +1,19 @@
+//
+//  SOCKit-iOS.h
+//  SOCKit-iOS
+//
+//  Created by Terry Lewis on 18/8/17.
+//  Copyright © 2017 RestKit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SOCKit-iOS.
+FOUNDATION_EXPORT double SOCKit_iOSVersionNumber;
+
+//! Project version string for SOCKit-iOS.
+FOUNDATION_EXPORT const unsigned char SOCKit_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SOCKit_iOS/PublicHeader.h>
+
+
