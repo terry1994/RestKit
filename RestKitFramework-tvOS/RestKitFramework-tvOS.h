77741989d3b38bc86c2c43417f496dc5bd47ea0a
Terry Lewis <terry1994dev@gmail.com>
2017-08-17 23:09:29 +1000

    More hacky tvOS stuff - with frameworks.

---
+++ RestKitFramework-tvOS/RestKitFramework-tvOS.h
@@ -0,0 +1,19 @@
+//
+//  RestKitFramework-tvOS.h
+//  RestKitFramework-tvOS
+//
+//  Created by Terry Lewis on 17/8/17.
+//  Copyright © 2017 RestKit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RestKitFramework-tvOS.
+FOUNDATION_EXPORT double RestKitFramework_tvOSVersionNumber;
+
+//! Project version string for RestKitFramework-tvOS.
+FOUNDATION_EXPORT const unsigned char RestKitFramework_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RestKitFramework_tvOS/PublicHeader.h>
+
+
